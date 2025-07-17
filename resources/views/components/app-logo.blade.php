ce2b847a2d96347b317f27ebabb33c99c1459e29
Victoria Lane <lane0085@algonquinlive.com>
2025-07-17 14:18:10 -0400

    Update nav bar on dashboard view

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,6 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+<div class="flex aspect-square size-8 items-center justify-center rounded-md text-accent-foreground">
+    <x-app-logo-icon class="size-5 fill-current" />
 </div>
-<div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold">Laravel Starter Kit</span>
+<div class="ms-1 grid flex-1 text-start">
+    <span class=" truncate leading-tight font-semibold text-white" style="font-family: 'Gochi Hand', cursive; font-weight: 400; font-size: 24px; padding-top: 4px;">Hive Mind</span>
 </div>
