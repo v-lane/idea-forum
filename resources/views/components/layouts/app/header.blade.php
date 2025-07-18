860e86a31a4c4b71fe1f74f2cd9ff151077c9c4b
Victoria Lane <lane0085@algonquinlive.com>
2025-07-18 11:52:42 -0400

    Update welcome page with navbar

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -12,52 +12,75 @@
 
             <flux:spacer />
 
-            <!-- Desktop User Menu -->
-            <flux:dropdown position="top" align="end">
-                <flux:profile
-                    circle
-                    avatar:color="auto"
-                    avatar:color:seed="{{ auth()->user()->initials() }}"
-                    class="cursor-pointer"
-                    :initials="auth()->user()->initials()"
-                />
+            @if (Route::has('login'))
+                <nav class="flex items-center justify-end gap-4">
+                    @auth
+                        <flux:dropdown position="top" align="end">
+                            <flux:profile
+                                circle
+                                avatar:color="auto"
+                                avatar:color:seed="{{ auth()->user()->initials() }}"
+                                class="cursor-pointer"
+                                :initials="auth()->user()->initials()"
+                            />
 
-                <flux:menu>
-                    <flux:menu.radio.group>
-                        <div class="p-0 text-sm font-normal">
-                            <div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
-                                <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
-                                    <span
-                                        class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black"
-                                    >
-                                        {{ auth()->user()->initials() }}
-                                    </span>
-                                </span>
+                            <flux:menu>
+                                <flux:menu.radio.group>
+                                    <div class="p-0 text-sm font-normal">
+                                        <div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
+                                            <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
+                                                <span
+                                                    class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black"
+                                                >
+                                                    {{ auth()->user()->initials() }}
+                                                </span>
+                                            </span>
 
-                                <div class="grid flex-1 text-start text-sm leading-tight">
-                                    <span class="truncate font-semibold">{{ auth()->user()->name }}</span>
-                                    <span class="truncate text-xs">{{ auth()->user()->email }}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </flux:menu.radio.group>
+                                            <div class="grid flex-1 text-start text-sm leading-tight">
+                                                <span class="truncate font-semibold">{{ auth()->user()->name }}</span>
+                                                <span class="truncate text-xs">{{ auth()->user()->email }}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </flux:menu.radio.group>
 
-                    <flux:menu.separator />
+                                <flux:menu.separator />
 
-                    <flux:menu.radio.group>
-                        <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
-                    </flux:menu.radio.group>
+                                <flux:menu.radio.group>
+                                    <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
+                                </flux:menu.radio.group>
 
-                    <flux:menu.separator />
+                                <flux:menu.separator />
 
-                    <form method="POST" action="{{ route('logout') }}" class="w-full">
-                        @csrf
-                        <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
-                            {{ __('Log Out') }}
-                        </flux:menu.item>
-                    </form>
-                </flux:menu>
-            </flux:dropdown>
+                                <form method="POST" action="{{ route('logout') }}" class="w-full">
+                                    @csrf
+                                    <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
+                                        {{ __('Log Out') }}
+                                    </flux:menu.item>
+                                </form>
+                            </flux:menu>
+                        </flux:dropdown>
+
+                    @else
+                        <a
+                            href="{{ route('login') }}"
+                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
+                        >
+                            Log in
+                        </a>
+
+                        @if (Route::has('register'))
+                            <a
+                                href="{{ route('register') }}"
+                                class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                                Register
+                            </a>
+                        @endif
+                    @endauth
+                </nav>
+            @endif
+
+
         </flux:header>
 
         {{ $slot }}
