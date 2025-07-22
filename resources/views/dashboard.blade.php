9d640b54b3093de3d937903e991dcbe68ea2dfcc
Victoria Lane <lane0085@algonquinlive.com>
2025-07-22 17:34:30 -0400

    Update dashboard and welcome page styles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.app :title="__('Dashboard')">
     <div class="flex items-center justify-center w-full">
-        <main class="flex-row w-full lg:max-w-6xl">
+        <main class="lg:max-w-6xl">
             <x-hero :text="'My Ideas'"/>
             <div class="m-3 bg-blue-400">ADD: + Create a New Idea</div>
             <livewire:idea-section :hasIdeaCount="'true'" />
