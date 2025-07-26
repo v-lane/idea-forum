9817934288bf7600b61e726e8f3af3c7a5125cac
Victoria Lane <lane0085@algonquinlive.com>
2025-07-26 08:15:10 -0400

    Remove svg icons

--- resources/views/livewire/idea-item.blade.php
+++ resources/views/livewire/idea-item.blade.php
@@ -14,15 +14,7 @@ p-6 border-pink-300 rounded-xl m-6 bg-white">
         </div>
         @if($idea['ideaId'] == 3)
         {{-- UPDATE IF LOGIC -> if user logged in == userId, show this --}}
-        {{-- ADD LINK/EVENT TO EDIT/DELETE --}}
-        {{-- DECIDE - SVG icons or flux icons --}}
         <div class="flex">
-            <div >
-                <x-edit-icon/>
-            </div>
-            <div class="ps-2 pe-6">
-                <x-delete-icon/>
-            </div>
             <div>
                <flux:icon.pencil-square class="text-zinc-700"/>
             </div>
@@ -37,14 +29,11 @@ p-6 border-pink-300 rounded-xl m-6 bg-white">
     </flux:text>
     <footer class="flex">
         {{-- ADD LISTENER/EVENT TO HEART ICON -> TO LIKE POST --}}
-        {{-- DECIDE - SVG icons or flux icons --}}
         <div class="flex pe-4 items-center">
-            <x-heart-icon/>
             <flux:icon.heart class="text-pink-600"/>
             <flux:text class="ps-1" color="pink">{{ $idea['numLikes'] }} Likes</flux:text>
         </div>
         <div class="flex items-center">
-            <x-comment-icon/>
             <flux:icon.chat-bubble-oval-left class="text-pink-600" />
             <flux:text class="ps-1" color="pink">{{ $idea['numComments']}} Comments</flux:textp>
         </div>
