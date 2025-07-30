7765c8ad3cee6bd1860530e807a51ed1a6c2127b
Victoria Lane <lane0085@algonquinlive.com>
2025-07-30 11:58:30 -0400

    Update save action when no changes to form

--- app/Livewire/IdeaForm.php
+++ app/Livewire/IdeaForm.php
@@ -20,24 +20,33 @@ class IdeaForm extends Component
 
     // Save function
     public function save () {
+
         $this->validate([
             'title' => 'required|max:255|min:2',
             'text' => 'required|min:4'
         ]);
 
-        $idea = !empty($this->id) ? Idea::find($this->id) : new Idea();
-        $idea->title = $this->title;
-        $idea->text = $this->text;
-        $idea->user_id = Auth::id();
-        $idea->save();
+        // if edit form and no changes to title, just close modal
+        if(!empty($this->id)) {
+            $idea = Idea::find($this->id);
+            if ($this->title == $idea->title && $this->text == $idea->text) {
+                Flux::modals()->close();
+            } else {
+                $idea = !empty($this->id) ? Idea::find($this->id) : new Idea();
+                $idea->title = $this->title;
+                $idea->text = $this->text;
+                $idea->user_id = Auth::id();
+                $idea->save();
 
-        $this->dispatch('saved');
-        Flux::modals()->close();
-        $this->id = "";
-        $this->title = "";
-        $this->text = "";
-        // Needs to be a 'saved' somewhere to trigger refresh (In livenote, livenote.blade.php has
-        // <livewire:note-form :key="$this->noteFormId" :$id :$title :$text @saved="refresh" />)
+                $this->dispatch('saved');
+                Flux::modals()->close();
+                $this->id = "";
+                $this->title = "";
+                $this->text = "";
+                // Needs to be a 'saved' somewhere to trigger refresh (In livenote, livenote.blade.php has
+                // <livewire:note-form :key="$this->noteFormId" :$id :$title :$text @saved="refresh" />)
+            }
+        }
     }
 
     public function render()
